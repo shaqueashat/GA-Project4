b0fd88f3949205ac80ccf7f5ebdf390c820c50da
Shaqueasha Thompkins <shaqueasha_thompkins@yahoo.com>
2020-03-27 10:34:00 -0400

    Updating .js file to add eventListener and function to flip game pieces

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+let piece = document.querySelectorAll(".game_pieces");
 
+function flipOver() {
+    this.classList.toggle('flip');
+}
+
+piece.forEach(piece => piece.addEventListener('onclick', flipOver) )
 
